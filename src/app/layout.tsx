17fa56fadee663e1e5d45f78fca7fa37c7a27ed8
jplate <jan.plate@gmail.com>
2024-05-15 17:44:41 +0200

    Favicon troubles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   title: "Pasi Diagram Editor [UNDER CONSTRUCTION]",
   description: "An editor for 'ontological' diagrams",
   icons: {
-    icon: "/vercel.svg"
+    icon: "/images/pasi_icon.svg"
   }
 };
 
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head><link rel="icon" href="/vercel.svg" sizes="any"/></head>
+      <head><link rel="icon" href="/images/pasi_icon.svg" sizes="any"/></head>
       <body className={lusitana.className+' antialiased'}>{children}</body>
     </html>
   );
