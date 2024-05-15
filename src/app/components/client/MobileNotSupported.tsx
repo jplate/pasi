5a26c1316ca65344adc52381db98a6e861b14308
jplate <jan.plate@gmail.com>
2024-05-15 20:51:56 +0200

    Update MobileNotSupported.tsx

--- src/app/components/client/MobileNotSupported.tsx
+++ src/app/components/client/MobileNotSupported.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MobileNotSupported: React.FC = () => {
     return (
-        <div className='text-center text-3xl text-white bg-slate-600 px-4 py-4 leading-loose'>
+        <div className='text-center text-lg text-white bg-slate-600 px-2 py-2 leading-relaxed'>
             <p><strong>Mobile device not supported.</strong></p>
             <p> Please access this web application from a laptop or desktop.</p>
         </div>
