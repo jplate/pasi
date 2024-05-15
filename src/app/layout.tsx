90fe8bd08390ed2998def7e81c816114ddd2bc1d
jplate <jan.plate@gmail.com>
2024-05-15 18:14:22 +0200

    Trying to use an svg as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   title: "Pasi Diagram Editor [UNDER CONSTRUCTION]",
   description: "An editor for 'ontological' diagrams",
   icons: {
-    icon: "/src/app/icon.ico"
+    icon: "/src/app/icon.svg"
   }
 };
 
