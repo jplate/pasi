50793a9dca949b60b207e769b7dfb3b6af027c63
jplate <jan.plate@gmail.com>
2024-05-19 04:41:40 +0200

    improved dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,30 +5,14 @@ import React, {useState, useEffect} from "react";
 import 'tippy.js/dist/tippy.css';
 
 
-
 const MainPanel = dynamic(() => import('./components/client/MainPanel.tsx'), {ssr: false,});
 
-function getInitialColorMode() { // Copied from Josh Comeau (https://www.joshwcomeau.com/react/dark-mode/)
-  const persistedColorPreference = window.localStorage.getItem('color-mode');
-  
-  const hasPersistedPreference = typeof persistedColorPreference === 'string';
-  // If the user has explicitly chosen light or dark,
-  // let's use it. Otherwise, this value will be null.
-  if (hasPersistedPreference) {
-    return persistedColorPreference;
-  }
-  // If they haven't been explicit, let's check the media
-  // query
-  const mql = window.matchMedia('(prefers-color-scheme: dark)');
-  const hasMediaQueryPreference = typeof mql.matches === 'boolean';
-  if (hasMediaQueryPreference) {
-    return mql.matches ? 'dark' : 'light';
-  }
-  // If they are using a browser/OS that doesn't support
-  // color themes, let's default to 'light'.
-  return 'light';
-}
+const getInitialColorScheme = () => {
+  const storedMode = localStorage.getItem('color-scheme');
+  const systemMode = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
+  return storedMode ?? systemMode;
+}
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
@@ -38,13 +22,19 @@ export default function Home() {
   useEffect(() => {
     Object.freeze(Object.prototype); // to prevent prototype pollution
     setIsMobile(matchMedia('(pointer:fine)').matches==false);
-    setIsDarkMode(getInitialColorMode()=='dark');  
+    setIsDarkMode(getInitialColorScheme()=='dark');  
   }, []);
 
   useEffect(() => {
     document.body.classList.remove(isDarkMode? 'light': 'dark');
     document.body.classList.add(isDarkMode? 'dark': 'light');
-    window.localStorage.setItem('color-mode', isDarkMode? 'dark': 'light');
+    window.localStorage.setItem('color-scheme', isDarkMode? 'dark': 'light');
+
+    const root = document.querySelector(':root');
+    if (root instanceof HTMLElement) {
+      root.style.setProperty('color-scheme', isDarkMode? 'dark': 'light');
+    }
+
   }, [isDarkMode]);
 
 
@@ -54,13 +44,13 @@ export default function Home() {
         <i><strong>pasi</strong></i>
         <button className='' onClick={() => {setIsDarkMode(!isDarkMode)}}>
           {isDarkMode ? (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"> {/* sun icon */}
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-            </svg>):(
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"> {/* moon icon */}
               <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+            </svg>):(
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"> {/* sun icon */}
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
             </svg>
-          )}
+        )}
         </button>
       </span>
     </div>
