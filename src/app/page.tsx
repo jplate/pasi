a61e61acd5faf6246d834fff2a3bdcff6d337881
jplate <jan.plate@gmail.com>
2024-05-19 05:32:09 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ const getInitialColorScheme = () => {
   return storedMode ?? systemMode;
 }
 
+
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -21,9 +22,12 @@ export default function Home() {
   
   useEffect(() => {
     Object.freeze(Object.prototype); // to prevent prototype pollution
+
     setIsMobile(matchMedia('(pointer:fine)').matches==false);
+
     setIsDarkMode(getInitialColorScheme()=='dark');  
   }, []);
+
 
   useEffect(() => {
     document.body.classList.remove(isDarkMode? 'light': 'dark');
@@ -75,7 +79,7 @@ export default function Home() {
               To create a diagram, start by selecting one or (with shift-click) more locations on the canvas below, and then click either the <strong>Node</strong> or 
               the <strong>Contour</strong> button. To manipulate your diagram, you can drag nodes around, add labels to nodes, connect nodes 
               with arrows, etc. When you’re done, click the <strong>Generate</strong> button to have the LaTeX code displayed in the grey area further below. 
-              To use that code in your document, you’ll need Peter Kabal’s <a href='https://ctan.org/pkg/texdraw'><code><i>texdraw</i></code></a> package.
+              To use that code in your document, you’ll need Peter Kabal’s <a href='https://ctan.org/pkg/texdraw' target='_blank'><code><i>texdraw</i></code></a> package.
               You can also load diagrams from previously generated code, using the <strong>Load</strong> button.
             </p>
           </section>
@@ -87,10 +91,10 @@ export default function Home() {
               The following are a few other editors that also export LaTeX code:
             </p>
             <ul className='list-disc translate-x-5'>
-              <li><a href='https://sourceforge.net/projects/dia-installer/?source=directory'><i>Dia</i></a>, a desktop application specializing on diagrams, with a wide variety of export options.</li>
-              <li><a href='https://inkscape.org/'><i>Inkscape</i></a>, a fully-featured desktop SVG editor, also allows the creation of diagrams.</li>
-              <li><a href='https://enjoysmath.github.io/quiver-bee/'><i>Quiver</i></a>, a powerful web-based editor that specializes on commutative diagrams.</li>
-              <li><a href='https://tpx.sourceforge.net/'><i>TpX</i></a>, another desktop application, superficially similar to Dia.</li>
+              <li><a href='https://sourceforge.net/projects/dia-installer/?source=directory' target='_blank'><i>Dia</i></a>, a desktop application specializing on diagrams, with a wide variety of export options.</li>
+              <li><a href='https://inkscape.org/' target='_blank'><i>Inkscape</i></a>, a fully-featured desktop SVG editor, also allows the creation of diagrams.</li>
+              <li><a href='https://enjoysmath.github.io/quiver-bee/' target='_blank'><i>Quiver</i></a>, a powerful web-based editor that specializes on commutative diagrams.</li>
+              <li><a href='https://tpx.sourceforge.net/' target='_blank'><i>TpX</i></a>, another desktop application, superficially similar to Dia.</li>
             </ul>
           </section>
         </div>
